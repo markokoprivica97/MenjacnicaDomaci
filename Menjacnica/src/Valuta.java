f6ab0eb336a5891657a8eec8b333df17f5005e94
markokoprivica97 <markokoprivica97@gmail.com>
2018-03-23 23:22:33 +0100

    prvideo

---
+++ Menjacnica/src/Valuta.java
@@ -0,0 +1,13 @@
+
+public class Valuta {
+private String nazivValute;
+
+public String getNazivValute() {
+	return nazivValute;
+}
+
+public void setNazivValute(String nazivValute) {
+	this.nazivValute = nazivValute;
+}
+
+}
