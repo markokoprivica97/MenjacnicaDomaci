7556ba36522aca276fcf1ab68975e4ee924363ff
markokoprivica97 <markokoprivica97@gmail.com>
2018-03-23 23:45:54 +0100

    prazna tela

--- Menjacnica/src/interfejs/MEnjacnica.java
+++ Menjacnica/src/interfejs/MEnjacnica.java
@@ -1,5 +1,24 @@
 package interfejs;
 
-public class MEnjacnica {
+public class MEnjacnica implements IntefejsMenjacnice{
+
+	@Override
+	public void dodajKurs(String skraceniNaziv, String naziv, int dan, int mesec, int godina, double srednji,
+			double kupovni, double prodajni) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void izbrisiKurs(String skraceniNaziv, int dan, int mesec, int godina) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public double nadjiKurs(String skraceniNaziv, int dan, int mesec, int godina) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
