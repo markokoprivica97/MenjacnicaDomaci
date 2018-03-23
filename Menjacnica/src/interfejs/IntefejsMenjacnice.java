079366fbc13a68e0505267be60186218ac24878e
markokoprivica97 <markokoprivica97@gmail.com>
2018-03-24 00:13:20 +0100

    izmenjen tip fje

--- Menjacnica/src/interfejs/IntefejsMenjacnice.java
+++ Menjacnica/src/interfejs/IntefejsMenjacnice.java
@@ -1,9 +1,11 @@
 package interfejs;
 
+import valuta.Valuta;
+
 public interface IntefejsMenjacnice {
 	void dodajKurs(String skraceniNaziv,String naziv,int dan,int mesec,int godina,double srednji,double kupovni,double prodajni);
 	void izbrisiKurs(String skraceniNaziv,int dan,int mesec,int godina);
-	double nadjiKurs(String skraceniNaziv,int dan,int mesec,int godina);
+Valuta nadjiKurs(String skraceniNaziv,int dan,int mesec,int godina);
 	
 	
 }
